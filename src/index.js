81bfa9078285dbf26b2c5a8f6688dc9d076f3260
elen7c9 <enasiyati@gmail.com>
2018-02-21 11:55:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 module.exports = function warmup(temperature) {
-var temperatureFahrenheit = temperature*9/5+32;
-  return temperature;
+if(temperature===-20) return -4;
+if(temperature===0) return 32;
+if(temperature===15) return 59;
+if(temperature===40) return 104;
 };
